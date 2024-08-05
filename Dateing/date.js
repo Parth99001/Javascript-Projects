1fdb8e6017b8a67bbc7d6ffec60517dc83aa2cb0
Parth99001 <parthgandhi350@gmail.com>
2024-08-05 17:44:52 +0530

    Add files via upload

---
+++ Dateing/date.js
@@ -0,0 +1,30 @@
+console.log("Hello");
+alert("Hello Parth I'm Priya");
+let first = prompt("");
+if (first =="") {
+  alert("Sorry Currently Bussy");
+}else{
+  alert("How Are You")
+  prompt("");
+  alert("Fine Its been a long time We had not done any chat!")
+  prompt("")
+  alert("Im currently in ahmedabad came for some office work")
+  prompt("")
+  alert("Yah its a great idea")
+  alert("Where shall go for dinner ?")
+  let  second= prompt("")
+if (second === "Yes") {
+  alert("I'll share you the location")
+  prompt("")
+  alert("Ok Will Meet at 8 PM sharp see you Bye")
+  prompt("")
+}else{
+  alert("Than where you want to go ?");
+  prompt("")
+  alert("Im hear only for 1 day we can not waste our time in Thearter")
+  prompt("")
+  alert("ok than will meet at 8 Pm sharp share me the location")
+  prompt("")
+  alert("Ok Bye see you at 8")
+}
+}
