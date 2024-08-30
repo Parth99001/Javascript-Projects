4baf7f7bcce5049a5291551a1531d430921c1467
Parth99001 <parthgandhi350@gmail.com>
2024-08-30 18:50:27 +0530

    Add files via upload

---
+++ BGCOLOR/color.js
@@ -0,0 +1,30 @@
+console.log("Hello");
+const slides=document.querySelectorAll(".sub");
+console.log(slides);
+let currentslide = 0;
+slides[currentslide].style.display="flex";
+
+function hid() {
+    slides.forEach((div)=>{
+        div.style.display="none";
+    })
+}
+
+function past() {
+    currentslide--;
+    if (currentslide<0) {
+        currentslide= slides.length-1;
+    }
+    hid();
+    slides[currentslide].style.display="flex";
+}
+
+function further() {
+    currentslide++;
+    if (currentslide >= slides.length) {
+        currentslide=0;
+    }
+    hid();
+    slides[currentslide].style.display="flex";
+}
+
